c888f75b50814a52b2084efd37788e225d779a34
Marcelo <marcelo.cepeda17@gmail.com>
2024-08-16 17:09:29 -0300

    Add RoutineRepository

---
+++ GymAppApi/Domain/Interfaces/IRoutineRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Domain.Entities;
+
+namespace Repositories
+{
+    public interface IRoutineRepository
+    {
+        IEnumerable<Rutine> GetAllRoutines();
+        Rutine GetRutineById(int id);
+        void AddRutine(Rutine rutine);
+        void UpdateRutine(Rutine rutine);
+        void DeleteRutine(int id);
+    }
+}
---
+++ GymAppApi/Infraestructure/Data/RoutineRepository.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories
+{
+    public class RoutineRepository : IRoutineRepository
+    {
+        private readonly DbContext _context;
+
+        public RoutineRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Rutine> GetAllRoutines()
+        {
+            return _context.Set<Rutine>().ToList();
+        }
+
+        public Rutine GetRutineById(int id)
+        {
+            return _context.Set<Rutine>().Find(id);
+        }
+
+        public void AddRutine(Rutine rutine)
+        {
+            _context.Set<Rutine>().Add(rutine);
+            _context.SaveChanges();
+        }
+
+        public void UpdateRutine(Rutine rutine)
+        {
+            _context.Set<Rutine>().Update(rutine);
+            _context.SaveChanges();
+        }
+
+        public void DeleteRutine(int id)
+        {
+            var rutine = _context.Set<Rutine>().Find(id);
+            if (rutine != null)
+            {
+                _context.Set<Rutine>().Remove(rutine);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
