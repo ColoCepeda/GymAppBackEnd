14a47efc31a70049d11308dae57791579151e205
Ivan-Porcari <ivanporcari93@gmail.com>
2024-08-19 21:33:09 -0300

    routine service and interface

---
+++ GymAppApi/Repository/Interfaces/IRoutineService.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IRoutineService
+    {
+        IEnumerable<Routine> GetAllRoutines();
+        Routine GetRoutineById(int id);
+        void AddRoutine(Routine routine);
+        void UpdateRoutine(Routine routine);
+        void DeleteRoutine(int id);
+
+
+    }
+}
